d87374b5ed5b520c1daf83408649572201bd4c90
senpai_sqrt2 <nextikden@gmail.com>
2025-03-19 17:12:49 +0300

    v1

---
+++ NOD.py
@@ -0,0 +1,17 @@
+print('Welcome to the Brain Games!')
+print('May I have your name?')
+name = input()
+print('Hello ' + name)
+print('Find the smallest common multiple of given numbers.')
+n1 = 6
+n2 = 9
+n3 = 13
+print('Question:', n1, n2, n3)
+answer = int(input())
+if answer == n1 * n2 * n3:
+    print('Your answer:', answer)
+    print('Correct!')
+elif answer != n1 * n2 * n3:
+    print('Your answer:', answer)
+    print(answer, 'is wrong answer ;(. Correct answer was', n1 * n2 * n3)
+    print('Let\'s try again, ' + name)
